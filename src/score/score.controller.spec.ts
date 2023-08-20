ee58c1feac39bba5d7ab5723788d1dad733af4ca
Kerrick Chan <kerrickchan@icloud.com>
2023-08-20 22:12:04 +0800

    make it more simple

--- src/score/score.controller.spec.ts
+++ src/score/score.controller.spec.ts
@@ -1,22 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ScoreController } from './score.controller';
 import { ScoreService } from './score.service';
-import { Score } from './entities/score.entity';
-
-const scoreArray = [
-  new Score(1, 'Strongly Disagree'),
-  new Score(2, 'Disagree'),
-  new Score(3, 'Neutral'),
-  new Score(4, 'Agree'),
-  new Score(5, 'Strongly Agree'),
-];
-
-const oneScore = new Score(3, 'Neutral');
-oneScore.id = 1;
 
 describe('ScoreController', () => {
   let controller: ScoreController;
-  let service: ScoreService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,23 +12,21 @@ describe('ScoreController', () => {
         {
           provide: ScoreService,
           useValue: {
-            findAll: jest.fn().mockResolvedValue(scoreArray),
-            findOne: jest.fn().mockResolvedValue(oneScore),
-            findOneBy: jest.fn().mockResolvedValue(oneScore),
-            create: jest.fn().mockReturnValue(oneScore),
-            update: jest.fn().mockResolvedValue({ affected: 1 }),
-            remove: jest.fn().mockResolvedValue({ affected: 1 }),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            findOneBy: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
           },
         },
       ],
     }).compile();
 
     controller = module.get<ScoreController>(ScoreController);
-    service = module.get<ScoreService>(ScoreService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-    expect(service).toBeDefined();
   });
 });
